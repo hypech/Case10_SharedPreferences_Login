084d6c3ff4bcd9b2cd53aac5982d84cce838c6d4
HYPech <TCTORBAO@GMAIL.COM>
2021-05-22 20:39:38 +0800

    Initial commit

---
+++ app/src/main/java/com/hypech/case8login_sharedpreferences/BusinessLogic.java
@@ -0,0 +1,14 @@
+package com.hypech.case8login_sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusinessLogic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_logic);
+    }
+}
